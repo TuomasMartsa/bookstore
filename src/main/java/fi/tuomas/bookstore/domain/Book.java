9b5641917ee6600e9d54cadf4e408185486e85b5
Käyttäjä <mikko.martikainen3@myy.haaga-helia.fi>
2020-09-17 22:03:04 +0300

    chapter 5

--- src/main/java/fi/tuomas/bookstore/domain/Book.java
+++ src/main/java/fi/tuomas/bookstore/domain/Book.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 
 @Entity
 public class Book {
@@ -17,6 +19,7 @@ public class Book {
 	private int price, year;
 	
 	@ManyToOne
+	@JsonManagedReference
 	@JoinColumn(name = "categoryid")
 	private Category category;
 
--- src/main/java/fi/tuomas/bookstore/web/bookController.java
+++ src/main/java/fi/tuomas/bookstore/web/bookController.java
@@ -1,5 +1,7 @@
 package fi.tuomas.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.tuomas.bookstore.domain.Book;
@@ -29,6 +32,20 @@ public class bookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	//REST to get all books
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	//REST to get book by id
+	@RequestMapping(value="/book/{id}", method= RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id") Long Bookid) {
+		return repository.findByid(Bookid);
+	}
+	
 	
 	@GetMapping("/add")
 	public String addBook(Model model) {
